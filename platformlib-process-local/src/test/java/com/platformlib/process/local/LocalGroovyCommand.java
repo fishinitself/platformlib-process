8407332045900bc630d84a9aee8b73999f67cf7e
vicspbsky <vicspbsky@gmail.com>
2022-09-10 17:51:42 +0300

    Update dependencies

--- platformlib-process-local/src/test/java/com/platformlib/process/local/LocalGroovyCommand.java
+++ platformlib-process-local/src/test/java/com/platformlib/process/local/LocalGroovyCommand.java
@@ -13,17 +13,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class LocalGroovyCommand {
     private static final Logger LOGGER = LoggerFactory.getLogger(LocalGroovyCommand.class);
-    private static final String GROOVY_JAR_RESOURCE_NAME = "/groovy/groovy-3.0.6.jar";
+    private static final String GROOVY_JAR_RESOURCE_NAME = "/groovy/groovy-3.0.12.jar";
     public static final Path GROOVY_JAR_PATH;
     private static final Map<String, Path> GROOVY_SCRIPS = new ConcurrentHashMap<>();
 
     static {
         try {
-            GROOVY_JAR_PATH = Paths.get(LocalGroovyCommand.class.getResource(GROOVY_JAR_RESOURCE_NAME).toURI());
+            GROOVY_JAR_PATH = Paths.get(Objects.requireNonNull(LocalGroovyCommand.class.getResource(GROOVY_JAR_RESOURCE_NAME), "Groovy library hasn't been found in project resources").toURI());
             if (!Files.isRegularFile(GROOVY_JAR_PATH)) {
                 throw new IllegalStateException("The groovy jar hasn't been found in resources at '"  + GROOVY_JAR_PATH + "'. Check that " + GROOVY_JAR_RESOURCE_NAME + " resource is acceptable in java");
             }
